e0f584f570766ef06a1babb1f261b7d6e985cadb
Yeliz <81921661+yelizsn@users.noreply.github.com>
2022-08-17 12:25:43 +0300

    download the file

---
+++ DataViewOrnek4(Telefon Kayıt)/DataViewOrnek4/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DataViewOrnek4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Model :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fiyat :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(61, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Telefon Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(174, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(338, 147);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Marka :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Markalar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 176);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(500, 103);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 290);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Telefon Veritabanı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DataViewOrnek4(Telefon Kayıt)/DataViewOrnek4/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataViewOrnek4
+{
+    public partial class Form1 : Form
+    {
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        DataView dataView;
+        string ConnectionString = "Provider=Microsoft.ACE.Oledb.12.0;Data Source=Telefon.accdb";
+        public Form1()
+        {
+            InitializeComponent();
+            Goruntule();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Telefon_Ekle(textBox1.Text,textBox2.Text,Convert.ToInt32(textBox3.Text));
+        }
+        DataTable dt = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Telefon_Ekle(string marka,string model, int fiyat)
+        {
+            
+            try
+            {
+                DataRowView newRow = dataView.AddNew();
+                newRow["Marka"] = marka;
+                newRow["Model"] = model;
+                newRow["Fiyat"] = fiyat;
+                newRow.EndEdit();
+                dataView.Sort = "Id";
+               
+                using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+                {
+                 
+                    OleDbCommand com = new OleDbCommand("INSERT INTO Telefon (Marka,Model,Fiyat) VALUES (@Marka,@Model,@Fiyat)");
+                    com.Connection = baglanti;
+
+                    baglanti.Open();
+                    if (baglanti.State == ConnectionState.Open)
+                    {
+                      
+                        com.Parameters.Add("@Marka", OleDbType.VarChar).Value = dataView[0][1];
+                        com.Parameters.Add("@Model", OleDbType.VarChar).Value = dataView[0][2];
+                        com.Parameters.Add("@Fiyat", OleDbType.Integer).Value = dataView[0][3];
+
+                        try
+                        {
+
+                            com.ExecuteNonQuery();
+                            MessageBox.Show("Telefon Eklendi");
+                            Goruntule();
+                        }
+                        catch (OleDbException ex)
+                        {
+                            MessageBox.Show(ex.Source);
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bağlantı Hatası");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+              
+            }
+        }
+
+        void Goruntule()
+        {
+            
+            using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+            {
+              
+                dataView = new DataView();
+                ds = new DataSet();
+                cmd = new OleDbCommand("SELECT * FROM Telefon", baglanti);
+                da = new OleDbDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(ds, "Add New");
+                dataView = new DataView(ds.Tables[0]);
+                dataView.Sort = "Id";
+
+                listBox1.Items.Clear();
+                
+                for (int i = 0; i < dataView.Count; i++)
+                {
+                    listBox1.Items.Add(dataView[i][1]);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataView.RowFilter = "Marka = '" + listBox1.SelectedItem.ToString()+"'"; 
+            dataGridView1.DataSource = dataView;
+
+        }
+    }
+}
